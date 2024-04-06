e47e7bafeaaccf5920b2e5029552e0e98fceb858
aartran <aartran@umich.edu>
2024-04-05 21:08:48 -0400

    prototype client server

--- src/sensor_srvcli/sensor_srvcli/MinimalClient.py
+++ src/sensor_srvcli/sensor_srvcli/MinimalClient.py
@@ -1,22 +1,40 @@
 import sys
 
-from sensor_srv.srv import GetLoad
+from sensor_interfacessensor_srvsensor_srv.srv import GetLoad
+from sensor_interfaces.msg import SensorData, SensorDataArray
+
 import rclpy
 from rclpy.node import Node
 
+from time import time
 
 class MinimalClientAsync(Node):
 
     def __init__(self):
         super().__init__('minimal_client_async')
-        self.cli = self.create_client(GetLoad, 'get_load_cell_data')
-        while not self.cli.wait_for_service(timeout_sec=1.0):
+        self._cli = self.create_client(GetLoad, 'get_load_cell_data')
+        while not self._cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
-        self.req = GetLoad.Request()
+        self._req = GetLoad.Request()
+        timer_period = 0.002 # seconds
+        self._timer = self.create_timer(timer_period, self.timer_callback)
+        self._publisher = self.create_publisher(SensorData, 'sensor', 10)
 
-    def send_request(self, num_datapoints: int):
-        self.req.num_datapoints = num_datapoints
-        self.future = self.cli.call_async(self.req)
+
+    def timer_callback(self):
+        now = time.time()
+        service_response = self.send_request(now)
+        
+        msg = SensorData()
+        msg.data = service_response.data
+        msg.timestamps = service_response.timestamps
+        
+        self._publisher.publish(msg)        
+        self.get_logger().info('Got sensor data')
+
+    def send_request(self, time: float):
+        self._req.req_time = time
+        self.future = self._cli.call_async(self._req)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
 
@@ -25,11 +43,10 @@ def main():
     rclpy.init()
 
     minimal_client = MinimalClientAsync()
-    response = minimal_client.send_request(int(sys.argv[1]), int(sys.argv[2]))
-    minimal_client.get_logger().info(
-        f"Retrieved data")
+    rclpy.spin(minimal_client)
 
-    minimal_client.destroy_node()
+    rclpy.shutdown()
+
     rclpy.shutdown()
 
 
--- src/sensor_srvcli/sensor_srvcli/MinimalServer.py
+++ src/sensor_srvcli/sensor_srvcli/MinimalServer.py
@@ -1,4 +1,4 @@
-from sensor_srv.srv import GetLoad
+from sensor_interfaces.srv import GetLoad
 
 import rclpy
 from rclpy.node import Node
@@ -8,34 +8,61 @@ import sys
 import numpy as np
 import time
 
-from utils.sensor_utils import SensorReader
+from utils import SensorReader
+from collections import deque 
+
+
 
 class MinimalService(Node):
 
     def __init__(self):
         super().__init__('minimal_service')
-        self.srv = self.create_service(GetLoad, 'get_load_cell_data', self.add_two_ints_callback)
+        self._srv = self.create_service(GetLoad, 'get_load_cell_data', self.getLoadCellData)
         timer_period = 0.01 # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self._timer = self.create_timer(timer_period, self.timer_callback)
 
+        #Declare default parameters
         self.declare_parameter('address', '127.0.0.3')
         self.declare_parameter('port', 10000)
         self.declare_parameter('num_samples', '10')
 
-
+        #Get parameters
         address = self.get_parameter('address').value
         port = self.get_parameter('port').value
+        num_samples = self.get_parmeter('num_samples').value
 
+        #Setup reader
         self._reader = SensorReader(address, port) 
-        self._reader.num_samples = self.get_parmeter('num_samples').value
+        self._reader.num_samples = num_samples
+
+        #Init deques
+        self.sensor_deque = deque()
+        self.sensor_timestamps = deque()
+
+        self.time_array = np.arange(num_samples+1, 1, step =-1)/2000
 
     def timer_callback(self):
-        self._reader.getData()
-        #TODO: Handle data from reader
+        times = time.time() + self.time_array
+        data = self._reader.getData()
+        self.sensor_deque.appendleft(reversed(data)) #assume that most recent data point in data is at data[-1]
+        self.sensor_timestamps.appendleft(times)
         self.get_logger().info('Got sensor data')
 
     def getLoadCellData(self, request, response):
-        #TODO: Handle data from reader
+        i = 0
+        while request.req_time < self.sensor_timestamps[i][0]:
+            i += 1
+        
+        num_pop = len(deque) - i + 1
+
+        data = None
+        timestamps = None
+        for _ in range(num_pop):
+            popped_data = self.sensor_deque.pop() 
+            popped_time = self.sensor_timestamps.pop() 
+            data = popped_data if data is None else np.hstack((popped_data,data))
+            timestamps = popped_time if timestamps is None else np.hstack((popped_time,timestamps))
+
         return response
 
 
