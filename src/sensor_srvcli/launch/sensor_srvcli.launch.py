d9df8118f9d3e5a438d078a1502a3f6d102975ee
aartran <aartran@umich.edu>
2024-04-06 15:50:49 -0400

    fix type error with num_samples

--- src/sensor_srvcli/launch/sensor_srvcli.launch.py
+++ src/sensor_srvcli/launch/sensor_srvcli.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
             name='minimal_service',
             parameters = [{'address': '127.0.0.3',
                            'port': 10000,
-                           'num_samples': 10
+                           'num_samples': '10'
             }]
         ),
         Node(
