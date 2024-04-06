de633ef473994fa204920e4eea2737418e28265b
aartran <aartran@umich.edu>
2024-04-05 21:32:08 -0400

    add launch file

--- src/sensor_srvcli/setup.py
+++ src/sensor_srvcli/setup.py
@@ -10,16 +10,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/sensor_srvcli.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
     maintainer_email='aartran@umich.edu',
-    description='TODO: Package description',
+    description='This package is designed to expose sensor data from a local server',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
+        'console_scripts': ['server = sensor_srvcli.MinimalServer:main',
+                            'client = sensor_srvcli.MinimalClient:main',
+                            'visualizer = sensor_srvcli.DataViz:main',
         ],
     },
 )
