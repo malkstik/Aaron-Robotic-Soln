f3de30dbe3a7ff6bcea17ea157a234121be612ee
aartran <aartran@umich.edu>
2024-04-06 15:52:36 -0400

    change publish and service request strategy to support multithreading

--- src/sensor_srvcli/sensor_srvcli/MinimalClient.py
+++ src/sensor_srvcli/sensor_srvcli/MinimalClient.py
@@ -5,6 +5,7 @@ from sensor_interfaces.msg import SensorData, SensorDataArray
 
 import rclpy
 from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
 
 from time import time
 
@@ -17,20 +18,30 @@ class MinimalClientAsync(Node):
             self.get_logger().info('service not available, waiting again...')
         self._req = GetLoad.Request()
         timer_period = 0.002 # seconds
-        self._timer = self.create_timer(timer_period, self.timer_callback)
+        self._servicetimer = self.create_timer(timer_period, self.service_timer_callback)
+        self._publishtimer = self.create_timer(timer_period, self.publish_timer_callback)
+        
         self._publisher = self.create_publisher(SensorData, 'sensor', 10)
 
+        self.msg = SensorData()
 
-    def timer_callback(self):
+
+    def service_timer_callback(self):
         now = time.time()
         service_response = self.send_request(now)
         
+        # Make new message
         msg = SensorData()
         msg.data = service_response.data
         msg.timestamps = service_response.timestamps
-        
-        self._publisher.publish(msg)        
+
+        # Update message
+        self.msg = msg
+
         self.get_logger().info('Got sensor data')
+
+    def publish_timer_callback(self):
+        self._publisher.publish(self.msg)
 
     def send_request(self, time: float):
         self._req.req_time = time
@@ -43,11 +54,18 @@ def main():
     rclpy.init()
 
     minimal_client = MinimalClientAsync()
-    rclpy.spin(minimal_client)
+    executor = MultiThreadedExecutor()
+    
+    
+    executor.add_node(minimal_client)
 
-    rclpy.shutdown()
 
-    rclpy.shutdown()
+    try:
+        executor.spin()
+    finally:
+        executor.shutdown()
+        minimal_client.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
